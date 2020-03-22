7e0ad4228e322e3c94b02d9a369335814dc81d40
Murillo <murillofnc@gmail.com>
2020-03-22 15:50:53 -0300

    arquivos antigos

---
+++ poo_avancada/evolucao_v2.py
@@ -0,0 +1,14 @@
+class EvolucaoHumana(object):
+    especie = ''
+
+    @staticmethod
+    def especies():
+        adjetivos = ('Habilis', 'Erectus', 'Neanderthalensis', 'Sapiens')
+        return ('Australopiteco',) + tuple(f'Homo {adj}' for adj in adjetivos)
+
+    @classmethod
+    def is_evoluido(cls):
+        return cls.especie == cls.especies()[-1]
+
+    def __init__(self, nome):
+        self.nome = nome
